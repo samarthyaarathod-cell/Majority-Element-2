35594e4bdb759066bd96a460e73907fed0c546f6
samarthyaarathod-cell <samarthya.a.rathod@gmail.com>
2026-03-31 12:51:13 +0530

    Add files via upload

---
+++ Majority_Element_2.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> majorityElement(vector<int>& nums) {
+    int count1 = 0, count2 = 0;
+    int candidate1 = 0, candidate2 = 1; // initialize differently
+
+    // Step 1: Find candidates
+    for (int num : nums) {
+        if (num == candidate1) {
+            count1++;
+        }
+        else if (num == candidate2) {
+            count2++;
+        }
+        else if (count1 == 0) {
+            candidate1 = num;
+            count1 = 1;
+        }
+        else if (count2 == 0) {
+            candidate2 = num;
+            count2 = 1;
+        }
+        else {
+            count1--;
+            count2--;
+        }
+    }
+
+    // Step 2: Verify candidates
+    count1 = count2 = 0;
+    for (int num : nums) {
+        if (num == candidate1) count1++;
+        else if (num == candidate2) count2++;
+    }
+
+    vector<int> result;
+    int n = nums.size();
+
+    if (count1 > n / 3) result.push_back(candidate1);
+    if (count2 > n / 3) result.push_back(candidate2);
+
+    return result;
+}
+
+int main() {
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    vector<int> nums(n);
+    cout << "Enter array elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+
+    vector<int> result = majorityElement(nums);
+
+    cout << "Elements appearing more than n/3 times: ";
+    for (int num : result) {
+        cout << num << " ";
+    }
+
+    return 0;
+}
